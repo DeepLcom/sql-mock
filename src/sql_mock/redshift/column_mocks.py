6418aeb181bfb9475b649c6aa8a8e5c615a96458
Thomas Schmidt <thomas.heinz.schmidt@gmail.com>
2024-01-07 12:57:56 +0100

    Make RedshiftMockTable work and add example

--- src/sql_mock/redshift/column_mocks.py
+++ src/sql_mock/redshift/column_mocks.py
@@ -5,68 +5,46 @@ class RedshiftColumnMock(ColumnMock):
     pass
 
 
-class ACLITEM(RedshiftColumnMock):
-    dtype = "ACLITEM"
+class SMALLINT(RedshiftColumnMock):
+    dtype = "SMALLINT"
+
+
+class INTEGER(RedshiftColumnMock):
+    dtype = "INTEGER"
+
+
+class BIGINT(RedshiftColumnMock):
+    dtype = "BIGINT"
+
+
+class DECIMAL(RedshiftColumnMock):
+    def __init__(self, default, precision, scale, nullable=False) -> None:
+        self.dtype = f"DECIMAL({precision}, {scale})"
+        super().__init__(default, nullable)
+
+
+class REAL(RedshiftColumnMock):
+    dtype = "REAL"
+
+
+class DOUBLE_PRECISION(RedshiftColumnMock):
+    dtype = "DOUBLE PRECISION"
 
 
 class BOOLEAN(RedshiftColumnMock):
     dtype = "BOOLEAN"
 
 
-class INT8(RedshiftColumnMock):
-    dtype = "INT8"
-
-
-class INT4(RedshiftColumnMock):
-    dtype = "INT4"
-
-
-class INT2(RedshiftColumnMock):
-    dtype = "INT2"
+class CHAR(RedshiftColumnMock):
+    dtype = "CHAR"
 
 
 class VARCHAR(RedshiftColumnMock):
     dtype = "VARCHAR"
 
 
-class OID(RedshiftColumnMock):
-    dtype = "OID"
-
-
-class REGPROC(RedshiftColumnMock):
-    dtype = "REGPROC"
-
-
-class XID(RedshiftColumnMock):
-    dtype = "XID"
-
-
-class FLOAT4(RedshiftColumnMock):
-    dtype = "FLOAT4"
-
-
-class FLOAT8(RedshiftColumnMock):
-    dtype = "FLOAT8"
-
-
-class TEXT(RedshiftColumnMock):
-    dtype = "TEXT"
-
-
-class CHAR(RedshiftColumnMock):
-    dtype = "CHAR"
-
-
 class DATE(RedshiftColumnMock):
     dtype = "DATE"
-
-
-class TIME(RedshiftColumnMock):
-    dtype = "TIME"
-
-
-class TIMETZ(RedshiftColumnMock):
-    dtype = "TIMETZ"
 
 
 class TIMESTAMP(RedshiftColumnMock):
@@ -77,7 +55,29 @@ class TIMESTAMPTZ(RedshiftColumnMock):
     dtype = "TIMESTAMPTZ"
 
 
-class NUMERIC(RedshiftColumnMock):
-    def __init__(self, default, precision, scale, nullable=False):
-        self.dtype = f"Decimal({precision}, {scale})"
-        super().__init__(default, nullable)
+class GEOMETRY(RedshiftColumnMock):
+    dtype = "GEOMETRY"
+
+
+class GEOGRAPHY(RedshiftColumnMock):
+    dtype = "GEOGRAPHY"
+
+
+class HLLSKETCH(RedshiftColumnMock):
+    dtype = "HLLSKETCH"
+
+
+class SUPER(RedshiftColumnMock):
+    dtype = "SUPER"
+
+
+class TIME(RedshiftColumnMock):
+    dtype = "TIME"
+
+
+class TIMETZ(RedshiftColumnMock):
+    dtype = "TIMETZ"
+
+
+class VARBYTE(RedshiftColumnMock):
+    dtype = "VARBYTE"
--- src/sql_mock/redshift/table_mocks.py
+++ src/sql_mock/redshift/table_mocks.py
@@ -16,7 +16,14 @@ class RedshiftMockTable(BaseMockTable):
         super().__init__(*args, **kwargs)
 
     def _get_results(self, query: str) -> list[dict]:
-        with redshift_connector.connect(**self.settings) as con:
+        with redshift_connector.connect(
+            host=self.settings.host,
+            database=self.settings.database,
+            user=self.settings.user,
+            password=self.settings.password,
+            port=self.settings.port,
+        ) as con:
             with con.cursor() as cursor:
-                res = cursor.fetch_dataframe
+                cursor.execute(query)
+                res = cursor.fetch_dataframe()
         return res.to_dict("records")
